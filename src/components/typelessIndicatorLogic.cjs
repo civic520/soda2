0e03d2b4d85a88c0f1c4977dbf511fe702b58187
m45801ch <m45801ch@gmail.com>
2026-08-12 13:44:53 +0800

    chore: remove obsolete .cjs helper files replaced by ESM versions

--- src/components/typelessIndicatorLogic.cjs
+++
@@ -1,7 +0,0 @@
-function indicatorClass(aiOptimizeRecording, cloudAsrActive, commandMode) {
-  if (aiOptimizeRecording) return "pill-ai";
-  if (commandMode) return "pill-command";
-  if (cloudAsrActive) return "pill-cloud";
-  return "pill-recording";
-}
-module.exports = { indicatorClass };
